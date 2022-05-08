bcc64d2d3eb38685fbba23e51cf0cf056051c4ec
vishnu <vishnuvishal900@gmail.com>
2022-05-08 20:27:07 +0530

    task for WeekEnd Completed

---
+++ src/StreamExample2.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+
+public class StreamExample2 {
+    public static void main(String[] args) {
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("java", 10021);map.put("python", 10022);map.put("struct", 10023);
+        map.put("clang", 10024);map.put("nodejs", 10025);map.put("mangodb", 10026);
+        map.put("html", 10027);map.put("css", 10028);map.put("javascript",10029);map.put("spring_boot", 10030);
+
+        Set<String> hs = new TreeSet<>();
+        hs.add("java");  hs.add("python");  hs.add("css");  hs.add("clang");  hs.add("html");  hs.add("nodejs");  hs.add("mangodb");
+
+        Set<String> s = map.keySet();s.removeAll(hs);
+        Set<String> h=  s.stream().collect(Collectors.toSet());
+        System.out.println("Keys exists in map but not in set are...");
+        System.out.println(h);
+        hs.addAll(h);
+        System.out.println("After adding the remaining keys to the set");
+        System.out.println(hs);
+    }
+}
---
+++ src/StreamExample3.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+    public class StreamExample3 {
+        public static void main(String[] args) {
+
+            Map<String, Integer> map = new HashMap<>();
+            Scanner sn=new Scanner(System.in);
+            System.out.println("enter the number of entries u wish to enter in MAP");
+            int p= sn.nextInt(),i=0;String s;
+
+            while(i<p) {   System.out.println("enter MAP key "+i+ " as String");s=sn.next();
+                if(map.containsKey(s)){
+                    System.out.println("KEy exists already..try another");
+                    continue;}
+                System.out.println("enter the value for above key..(int)");
+                int value=sn.nextInt();
+                map.put(s, value);i++;
+            }
+            Set<String> st = map.keySet();
+
+            Set<String> hs = new TreeSet<>();
+            i=0;System.out.println("Enter the no of entries u need to have in Set");
+            System.out.println("Enter value less than"+st.size());
+            int size=sn.nextInt();
+            if(size<=st.size()) {
+                while (i < size) { System.out.println("Entered a key from the given options "+st);
+                    s = sn.next();
+                    if (st.contains(s)){ hs.add(s);
+                    i++;}
+                    else{ System.out.println("Entered key does not exist in given map.. try again");
+                    }
+                }
+            } else System.out.println("pls Enter value less than"+st.size());
+
+            st.removeAll(hs);
+            Set<String> h=  st.stream().collect(Collectors.toSet());
+            System.out.println("Keys exists in map but not in set are...");
+            System.out.println(h);
+            hs.addAll(h);
+
+            System.out.println("After adding the remaining keys to the set");
+            System.out.println(hs);
+        }
+    }
+
+
